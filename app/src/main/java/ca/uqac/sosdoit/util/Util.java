bdf71f232c9369d6c6c5c3fea3804199f4a217ef
Tugdwal <tudal.le.bot@gmail.com>
2017-11-09 10:41:57 -0500

    [FIX][METHOD] Util.hideKeyboard

--- app/src/main/java/ca/uqac/sosdoit/util/Util.java
+++ app/src/main/java/ca/uqac/sosdoit/util/Util.java
@@ -18,6 +18,6 @@ public abstract class Util
     public static void hideKeyboard(Activity activity, View view)
     {
         InputMethodManager imm = (InputMethodManager) activity.getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_IMPLICIT_ONLY);
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
