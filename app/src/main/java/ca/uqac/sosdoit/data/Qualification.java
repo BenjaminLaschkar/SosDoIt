0960c78e2c9b699e7902d55aaa8d8f436b66a659
thomas <thomas.faget@etu.enseeiht.fr>
2017-11-10 11:43:22 -0500

    Improve Task and Qualification to add a text associated

--- app/src/main/java/ca/uqac/sosdoit/data/Qualification.java
+++ app/src/main/java/ca/uqac/sosdoit/data/Qualification.java
@@ -1,12 +1,27 @@
 package ca.uqac.sosdoit.data;
 
 /**
- * Created by thomas on 06/11/2017.
+ * The list of all the Qualification possible for an advert
  */
 
 public enum Qualification {
 
-    TEST
-
+    TEST                ("Test"),
+    MATH_ACKNOWLEDGE    ("Math Acknowledge");
     // TODO
+
+    private String text = "";
+
+    Qualification(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
 }
--- app/src/main/java/ca/uqac/sosdoit/data/Task.java
+++ app/src/main/java/ca/uqac/sosdoit/data/Task.java
@@ -1,13 +1,33 @@
 package ca.uqac.sosdoit.data;
 
 /**
- *
+ * The list of all the Task possible for an advert
  */
 
 public enum Task {
 
-    TEST,
-    BABYSITTING,
-    MATH_COURSE
+    // List of tasks :
+    TEST            ("test"),
+    BABYSITTING     ("Babysitting"),
+    MATH_COURSE     ("Math course");
     // TODO
+
+    private String text = "";
+
+    Task(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+
+
+
 }
