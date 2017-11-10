4ba1841c0c0a2e96171b58a30ac653b3e249ace7
thomas <thomas.faget@etu.enseeiht.fr>
2017-11-10 11:44:59 -0500

    Apply User change

--- app/src/main/java/ca/uqac/sosdoit/ProfileActivity.java
+++ app/src/main/java/ca/uqac/sosdoit/ProfileActivity.java
@@ -3,7 +3,6 @@ package ca.uqac.sosdoit;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -49,7 +48,7 @@ public class ProfileActivity extends AppCompatActivity implements IDatabaseManag
     @Override
     public void call(User user)
     {
-        username.setText(user.getPseudo());
+        username.setText(user.getUsername());
         firstName.setText(user.getFirstname());
         lastName.setText(user.getLastname());
         email.setText(auth.getCurrentUser().getEmail());
