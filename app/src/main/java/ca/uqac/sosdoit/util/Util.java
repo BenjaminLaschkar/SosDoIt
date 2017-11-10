295cf7161bc4711ed78cf275cdf9788d4c2b09a2
thomas <thomas.faget@etu.enseeiht.fr>
2017-11-10 17:22:06 -0500

    Add methods for rating and distance between 2 locations

--- app/src/main/java/ca/uqac/sosdoit/util/Util.java
+++ app/src/main/java/ca/uqac/sosdoit/util/Util.java
@@ -2,12 +2,19 @@ package ca.uqac.sosdoit.util;
 
 import android.app.Activity;
 import android.content.Context;
+import android.location.Location;
 import android.support.design.widget.TextInputLayout;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+import ca.uqac.sosdoit.data.Rating;
 
 public final class Util
 {
@@ -68,4 +75,32 @@ public final class Util
         @Override
         public void afterTextChanged(Editable s) {}
     }
+
+
+    /** Get the mean value from a list of Rating
+     *
+     * @param ratingList a list of rating of an user
+     * @return the mean of the rating
+     */
+    public static double getFinalRating(List<Rating> ratingList) {
+        double nbRatings = ratingList.size();
+        double mean = 0.0;
+        for (Rating rating: ratingList) {
+            mean += rating.getRate();
+        }
+        return mean;
+    }
+
+    /** Get the distance between two locations (in kilometers)
+     *
+     * @param location1 location 1, not null
+     * @param location2 location 2, not null
+     * @return the distance between the two location (in kilometers)
+     */
+    public static float distanceBetweenTowLocation(LatLng location1, LatLng location2) {
+        float[] results = new float[1];
+        Location.distanceBetween(location1.latitude, location1.longitude, location2.latitude, location2.longitude, results);
+        return results[0];
+    }
+
 }
